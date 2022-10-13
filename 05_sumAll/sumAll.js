a8df2f993535075df0822bc0b1ed128719456a25
Oliver L <oliver.lkm@gmail.com>
2022-10-13 17:28:16 +1100

    Complete exercise and unit testing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (...args) {
+  const [start, end] = args.sort((a, b) => a - b);
 
+  if (start < 0 || end < 0) return "ERROR";
+
+  if (typeof start !== "number" || typeof end !== "number") return "ERROR";
+
+  let sum = 0;
+
+  for (i = start; i < end + 1; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  const celsius = (fahrenheit - 32) * (5 / 9);
+  const celsiusRounded = Math.round(celsius * 10) / 10;
+  return celsiusRounded;
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  const fahrenheitRounded = Math.round(fahrenheit * 10) / 10;
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
